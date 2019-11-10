2d6240b24629c7e8e3878352c10b0861787e746e
P. Maino <pcmaino@gmail.com>
2019-11-09 19:30:45 -0500

    Added basic functionality and assets. Hazard set spawning works!

---
+++ Assets/Scripts/FrogMotion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMotion : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 4;
+
+    [SerializeField]
+    private float shakeModifier = 1;
+
+    [SerializeField]
+    private float shakeIncrease = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame. FixedUpdate is used for Physics stuff.
+    void FixedUpdate()
+    {
+        //Adds a bit of random shake to the object.
+        float randShakeX = Random.Range(shakeModifier * -1f, shakeModifier * 1f);
+        float randShakeY = Random.Range(shakeModifier * -1f, shakeModifier * 1f);
+        gameObject.transform.Translate(randShakeX * Time.deltaTime, randShakeY * Time.deltaTime, 0);
+
+        shakeModifier += shakeIncrease * Time.deltaTime;
+        manageInput();
+
+    }
+
+
+    void manageInput()
+    {
+        float move = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(Vector2.right * move);
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject groundObject;
+
+    [SerializeField]
+    private float globalSpeed = 0.005f;
+
+    [SerializeField]
+    private List<GameObject> objects = new List<GameObject>();
+
+    private GameObject currHazardSet1;
+    private GameObject currHazardSet2;
+
+    private float spawnBoundTop = 16f;
+    private float spawnBoundNewSignal = 0f;
+    private float spawnBoundBottom = -6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initHazardSets();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Initially moves the ground away
+        if(groundObject != null)
+        {
+            groundObject.transform.Translate(Vector3.down * globalSpeed * Time.deltaTime);
+            if(groundObject.transform.position.y <= -spawnBoundBottom)
+            {
+                Destroy(groundObject);
+                groundObject = null;
+            }
+        }
+
+        //Moves the current sets of hazards down
+        currHazardSet1.transform.Translate(Vector3.down * globalSpeed * Time.deltaTime);
+        if(currHazardSet2 != null) {
+            currHazardSet2.transform.Translate(Vector3.down * globalSpeed * Time.deltaTime);
+        }
+
+        //if that hazard set has gone too low, spawn a new one
+        if(currHazardSet1.transform.position.y <= spawnBoundNewSignal && currHazardSet2 == null)
+        {
+            newHazardSet();
+        }
+
+        if (currHazardSet1.transform.position.y <= spawnBoundBottom)
+        {
+            Destroy(currHazardSet1);
+            currHazardSet1 = currHazardSet2;
+            currHazardSet2 = null;
+        }
+    }
+
+    void newHazardSet() {
+        if(currHazardSet2 != null)
+        {
+            //currHazardSet1 = currHazardSet2;
+        }
+
+        currHazardSet2 = (GameObject)Instantiate(objects[Random.Range(0, objects.Count)]);
+        currHazardSet2.transform.position = new Vector2(0, spawnBoundTop);
+    }
+
+    void initHazardSets()
+    {
+        currHazardSet1 = Instantiate(objects[Random.Range(0, objects.Count)]);
+        currHazardSet1.transform.position = new Vector2(0, spawnBoundTop);
+        currHazardSet2 = null;
+    }
+}
